0f11b69e496f6e71dec6faa3bfee2efa93bcb368
Shadypio <enzuccioamendola@gmail.com>
2022-10-05 10:18:25 +0200

    Modified the voice speed

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,13 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.say("Hello world")
-engine.runAndWait()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+newVoiceRate = 190  # specifies the speed of the voice
+engine.setProperty('rate', newVoiceRate)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak("This is Shady")

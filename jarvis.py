ce053f32686c797870cc298a860b2793b4b76284
Shadypio <enzuccioamendola@gmail.com>
2022-10-05 10:28:07 +0200

    Added time and date function

--- jarvis.py
+++ jarvis.py
@@ -1,13 +1,31 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-newVoiceRate = 190  # specifies the speed of the voice
+newVoiceRate = 150  # specifies the speed of the voice
 engine.setProperty('rate', newVoiceRate)
 
+# starts the talk
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("This is Shady")
+# tells the current time
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak(Time)
+
+# tells the current date
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak("La data di oggi è")
+    speak(day)
+    speak(month)
+    speak(year)
+
+speak("Buongiorno a tutti")
+date()
